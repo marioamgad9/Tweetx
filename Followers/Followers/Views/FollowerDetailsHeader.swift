db96f3472fda1b13732dc26a32726a877c9e71e4
Mario Mouris <marioamgad9@gmail.com>
2021-02-03 18:42:58 +0200

    Design rest of FollowerDetailsHeader

--- Followers/Followers/Views/FollowerDetailsHeader.swift
+++ Followers/Followers/Views/FollowerDetailsHeader.swift
@@ -27,16 +27,60 @@ class FollowerDetailsHeader: NiblessView {
         return view
     }()
     
+    private let profilePictureImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        imageView.layer.cornerRadius = 45
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = Color.offWhite.value.cgColor
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.bold(size: .huge, scalable: true)
+        label.textColor = Color.darkestGrey.value
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let handleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.regular(size: .small, scalable: true)
+        label.textColor = Color.lightGrey.value
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let bioLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.regular(size: .normal, scalable: true)
+        label.textColor = Color.darkestGrey.value
+        label.numberOfLines = 0
+        return label
+    }()
+    
     // MARK: - Methods
     func configure(with viewModel: FollowerDetailsHeaderViewModel) {
         backgroundImageView.sd_setImage(with: viewModel.backgroundImageURL,
                                             placeholderImage: UIImage(named: "background-placeholdepr"))
+        
+        profilePictureImageView.sd_setImage(with: viewModel.profilePictureURL,
+                                            placeholderImage: UIImage(named: "profile-placeholder"))
+        
+        nameLabel.text = viewModel.name
+        handleLabel.text = viewModel.twitterHandle
+        bioLabel.text = viewModel.bio
     }
     
     override func configureViewHierarchy() {
         // Add background image view & overlay
         add(backgroundImageView, then: {
-            $0.fillSuperview()
+            $0.anchor(.leading(leadingAnchor),
+                      .top(topAnchor),
+                      .trailing(trailingAnchor))
             $0.constrainHeight(150)
         })
         
@@ -46,6 +90,40 @@ class FollowerDetailsHeader: NiblessView {
                       .trailing(backgroundImageView.trailingAnchor),
                       .bottom(backgroundImageView.bottomAnchor))
         })
+        
+        // Add profile picture image view
+        add(profilePictureImageView, then: {
+            $0.anchor(.leading(leadingAnchor, constant: 16))
+            $0.centerYTo(backgroundImageView.bottomAnchor)
+            $0.constrainHeight(90)
+            $0.constrainWidth(90)
+        })
+        
+        // Add name label
+        add(nameLabel, then: {
+            $0.anchor(.leading(leadingAnchor, constant: 16),
+                      .top(profilePictureImageView.bottomAnchor, constant: 5),
+                      .trailing(trailingAnchor, constant: -16))
+        })
+        
+        // Add handle label
+        add(handleLabel, then: {
+            $0.anchor(.leading(leadingAnchor, constant: 16),
+                      .top(nameLabel.bottomAnchor, constant: 2),
+                      .trailing(trailingAnchor, constant: -16))
+        })
+        
+        // Add bio label
+        add(bioLabel, then: {
+            $0.anchor(.leading(leadingAnchor, constant: 16),
+                      .top(handleLabel.bottomAnchor, constant: 8),
+                      .trailing(trailingAnchor, constant: -16),
+                      .bottom(bottomAnchor, constant: -16))
+        })
+    }
+    
+    override func viewHierarchyDidConfigure() {
+        backgroundColor = Color.offWhite.value
     }
 }
 
