ae6131321da7be68da4cb2996c89b03a6716cded
Mario Mouris <marioamgad9@gmail.com>
2021-02-03 14:32:12 +0200

    Create empty Followers module and link it with rest of modules

---
+++ Followers/Followers/Followers.h
@@ -0,0 +1,18 @@
+//
+//  Followers.h
+//  Followers
+//
+//  Created by Mario Mouris on 03/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Followers.
+FOUNDATION_EXPORT double FollowersVersionNumber;
+
+//! Project version string for Followers.
+FOUNDATION_EXPORT const unsigned char FollowersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Followers/PublicHeader.h>
+
+
